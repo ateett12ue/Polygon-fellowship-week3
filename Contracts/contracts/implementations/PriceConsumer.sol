04fc11053b066b731098e5d65f237c2254712117
ateet <ateet@propelld.com>
2022-07-02 19:16:44 +0530

    sudo koven oracle

---
+++ Contracts/contracts/implementations/PriceConsumer.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "../interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumer {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
